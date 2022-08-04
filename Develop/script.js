a368cd0c80f209d5f105320d4b5933d13044db27
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-08-03 15:17:47 -1000

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,3 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +12,53 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+// Assignment code here
+//define characters for password generator
+
+
+function writePassword(){
+//length of password?
+var passwordLength;
+passwordLength = prompt ("Password Length?", undefined, _default,8); 
+//is length between 8 and 128 characters?
+if (passwordLength > 7) {
+ return;
+}
+//if password is not correct length reprompt
+else if (passwordLength < 8) {
+  passwordLength = prompt ("Password Length?", undefined, _default,8)
+}
+//pick if they want lower case
+var lowCase;
+lowCase = confirm ("yes or no?");
+
+//pick if they want upper case
+var upCase;
+upCase = confirm ("yes or no?");
+//pick if they want approved characters
+var otherCharacters;
+otherCharacters = confirm ("yes or no?");
+//pick if they want numbers
+var numBERS;
+numBERS = confirm ("yes or no?");
+
+//need to add selected characters to the password
+for (var i = 0; i < Rpassword.length; i++) {
+  Rpassword = Rpassword +   passwordcharacters;
+
+}
+return Rpassword;
+
+var passwordcharacters = "abcde1234!?";
+var Rpassword = '';
+
+
+
+}
+console.log(writePassword());
+
